a418166493facacd9c015a149d2478021ed62d3b
Na Hyung Choi and saswati patra <saswati@>
2019-06-05 09:47:39 -0700

    format postcard, add stamp, start to link input address to postcard

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
   $("#name-form").submit(function(event) {
     var inputName = $("input#name").val();
+    var inputAddress1 = $("input#address1").val();
+    var inputAddress2 = $("input#address2").val();
+    var inputCity = $("input#city").val();
+    var inputState = $("input#state").val();
+    var inputZip = $("input#zip").val();
+    var inputCountry = $("input#country").val();
 
     $(".name").text(inputName);
+    $(".address1").text(inputAddress1);
+    $(".address2").text(inputAddress2);
+    $(".city").text(inputCity);
+    $(".state").text(inputState);
+    $(".zip").text(inputZip);
+    $(".country").text(inputCountry);
 
     $("#letter").show();
 
